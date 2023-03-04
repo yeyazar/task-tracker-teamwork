03058c56e5eb98c10a4ac5851ce565c594125d49
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-03-04 15:17:38 +0100

    APIadded

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -2,23 +2,26 @@ const AddTask = () => {
   return (
     <div>
       <form className="form-group">
-        <div className="form-group text-left mt-3">
-          <label for="addTask">Task</label>
-          <select className="form-control" id="addTask">
-            <option hidden>Add Task</option>
-            <option>Work Out</option>
-            <option>Study Project</option>
-            <option>Cook Food</option>
-          </select>
+
+        <div className="form-group text-start">
+          <label htmlFor="formGroupExampleInput">Add Task</label>
+          <input
+            type="text"
+            className="form-control"
+            id="addTask"
+            placeholder="Add Task"
+          />
         </div>
-        <div className="form-group text-left mt-3">
-          <label for="dayTime">Day & Time</label>
-          <select className="form-control" id="dayTime">
-            <option hidden>Add Day & Time</option>
-            <option>Mar 5th at 15:30</option>
-            <option>Mar 7th at 21:30</option>
-          </select>
+        <div className="form-group text-start">
+          <label htmlFor="formGroupExampleInput2">Day & Time</label>
+          <input
+            type="text"
+            className="form-control"
+            id="dayTime"
+            placeholder="Add Day & Time"
+          />
         </div>
+
         <button type="submit" className="btn btn-info mt-3 w-100">
           Save Task
         </button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Dugme from "../components/Button";
 import AddTask from "../components/AddTask";
 import Tasks from "../components/Tasks";
+import axios from "axios";
 
 const Home = () => {
+  const BASE_URL = "https://63fa2bf8beec322c57eeb667.mockapi.io/tasks";
   const [showAddTask, setShowAddTask] = useState(false);
 
   const showClose = () => {
     setShowAddTask(!showAddTask);
   };
 
+  const getTasks = async () => {
+    try {
+      const { data } = await axios(BASE_URL);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
   return (
-    <div className="bg-warning text-center d-flex justify-content-center" >
+    <div className="bg-warning text-center d-flex justify-content-center">
       <div className="p-5 border border-primary bg-light d-flex flex-column m-4 w-50">
         <Header />
         <Dugme visible={showAddTask} onShow={showClose} />
